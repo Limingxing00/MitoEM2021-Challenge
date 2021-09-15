dddb388a4aab004fa577058b53c39266e304fc03
anonymous <641475073@qq.com>
2021-09-15 16:03:22 +0800

    update annotation

--- eval_iter.sh
+++ eval_iter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH=/opt/conda/bin:$PATH &&\
-cd /braindat/lab/limx/MitoEM2021/CODE/HUMAN/rsunet_retrain_297000_v2 &&\
+cd /braindat/lab/limx/MitoEM2021/CODE/HUMAN/rsunet_retrain_297000_v2 &&\   # You can change the root path for your PC
 python connectomics/utils/evaluation/iteration_eval.py
 
 
