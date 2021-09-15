cda06c69f4cf64cdf59eb6b05ab013ea201662e5
anonymous <641475073@qq.com>
2021-09-15 15:53:12 +0800

    Annotate to the cc attention code
    because it is not used

--- connectomics/model/block/basic.py
+++ connectomics/model/block/basic.py
@@ -1,10 +1,11 @@
-import os,sys
+import os, sys
 import torch
 import math
 import torch.nn as nn
 import torch.nn.functional as F
 
 from ..norm import *
+
 
 # common layers
 def get_functional_act(mode='relu'):
@@ -18,6 +19,7 @@ def get_functional_act(mode='relu'):
     }
     return activation_dict[mode]
 
+
 def get_layer_act(mode=''):
     if mode == '':
         return []
@@ -30,58 +32,59 @@ def get_layer_act(mode=''):
         return [nn.LeakyReLU(inplace=True, negative_slope=float(mode[5:]))]
     raise ValueError('Unknown activation layer option {}'.format(mode))
 
+
 def get_layer_norm(out_planes, norm_mode='', dim=2):
-    if norm_mode=='':
+    if norm_mode == '':
         return []
-    elif norm_mode=='bn':
-        if dim==1:
+    elif norm_mode == 'bn':
+        if dim == 1:
             return [SynchronizedBatchNorm1d(out_planes)]
-        elif dim==2:
+        elif dim == 2:
             return [SynchronizedBatchNorm2d(out_planes)]
-        elif dim==3:
+        elif dim == 3:
             return [SynchronizedBatchNorm3d(out_planes)]
-    elif norm_mode=='abn':
-        if dim==1:
+    elif norm_mode == 'abn':
+        if dim == 1:
             return [nn.BatchNorm1d(out_planes)]
-        elif dim==2:
+        elif dim == 2:
             return [nn.BatchNorm2d(out_planes)]
-        elif dim==3:
+        elif dim == 3:
             return [nn.BatchNorm3d(out_planes)]
-    elif norm_mode=='in':
-        if dim==1:
+    elif norm_mode == 'in':
+        if dim == 1:
             return [nn.InstanceNorm1d(out_planes)]
-        elif dim==2:
+        elif dim == 2:
             return [nn.InstanceNorm2d(out_planes)]
-        elif dim==3:
+        elif dim == 3:
             return [nn.InstanceNorm3d(out_planes)]
-    elif norm_mode=='bin':
-        if dim==1:
+    elif norm_mode == 'bin':
+        if dim == 1:
             return [BatchInstanceNorm1d(out_planes)]
-        elif dim==2:
+        elif dim == 2:
             return [BatchInstanceNorm2d(out_planes)]
-        elif dim==3:
+        elif dim == 3:
             return [BatchInstanceNorm3d(out_planes)]
     raise ValueError('Unknown normalization norm option {}'.format(mode))
 
 
 # conv basic blocks
-def conv2d_norm_act(in_planes, out_planes, kernel_size=(3,3), stride=1, 
-                  dilation=(1,1), padding=(1,1), bias=True, pad_mode='rep', norm_mode='', act_mode='', return_list=False):
-
-    if isinstance(padding,int):
-        pad_mode = pad_mode if padding!=0 else 'zeros'
+def conv2d_norm_act(in_planes, out_planes, kernel_size=(3, 3), stride=1,
+                    dilation=(1, 1), padding=(1, 1), bias=True, pad_mode='rep', norm_mode='', act_mode='',
+                    return_list=False):
+    if isinstance(padding, int):
+        pad_mode = pad_mode if padding != 0 else 'zeros'
     else:
-        pad_mode = pad_mode if max(padding)!=0 else 'zeros'
+        pad_mode = pad_mode if max(padding) != 0 else 'zeros'
 
-    if pad_mode in ['zeros','circular']:
+    if pad_mode in ['zeros', 'circular']:
         layers = [nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size,
-                     stride=stride, padding=padding, padding_mode=pad_mode, dilation=dilation, bias=bias)] 
-    elif pad_mode=='rep':
+                            stride=stride, padding=padding, padding_mode=pad_mode, dilation=dilation, bias=bias)]
+    elif pad_mode == 'rep':
         # the size of the padding should be a 6-tuple        
         padding = tuple([x for x in padding for _ in range(2)][::-1])
         layers = [nn.ReplicationPad2d(padding),
                   nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size,
-                     stride=stride, padding=0, dilation=dilation, bias=bias)]
+                            stride=stride, padding=0, dilation=dilation, bias=bias)]
     else:
         raise ValueError('Unknown padding option {}'.format(mode))
     # layers += get_layer_norm(out_planes, norm_mode)
@@ -91,23 +94,24 @@ def conv2d_norm_act(in_planes, out_planes, kernel_size=(3,3), stride=1,
     else:
         return nn.Sequential(*layers)
 
-def conv3d_norm_act(in_planes, out_planes, kernel_size=(3,3,3), stride=1, 
-                  dilation=(1,1,1), padding=(1,1,1), bias=True, pad_mode='rep', norm_mode='', act_mode='', return_list=False):
-    
-    if isinstance(padding,int):
-        pad_mode = pad_mode if padding!=0 else 'zeros'
-    else:
-        pad_mode = pad_mode if max(padding)!=0 else 'zeros'
 
-    if pad_mode in ['zeros','circular']:
+def conv3d_norm_act(in_planes, out_planes, kernel_size=(3, 3, 3), stride=1,
+                    dilation=(1, 1, 1), padding=(1, 1, 1), bias=True, pad_mode='rep', norm_mode='', act_mode='',
+                    return_list=False):
+    if isinstance(padding, int):
+        pad_mode = pad_mode if padding != 0 else 'zeros'
+    else:
+        pad_mode = pad_mode if max(padding) != 0 else 'zeros'
+
+    if pad_mode in ['zeros', 'circular']:
         layers = [nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size,
-                     stride=stride, padding=padding, padding_mode=pad_mode, dilation=dilation, bias=bias)] 
-    elif pad_mode=='rep':
+                            stride=stride, padding=padding, padding_mode=pad_mode, dilation=dilation, bias=bias)]
+    elif pad_mode == 'rep':
         # the size of the padding should be a 6-tuple        
         padding = tuple([x for x in padding for _ in range(2)][::-1])
         layers = [nn.ReplicationPad3d(padding),
                   nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size,
-                     stride=stride, padding=0, dilation=dilation, bias=bias)]
+                            stride=stride, padding=0, dilation=dilation, bias=bias)]
     else:
         raise ValueError('Unknown padding option {}'.format(mode))
 
@@ -238,127 +242,122 @@ class NONLocalBlock3D(_NonLocalBlockND):
                                               bn_layer=bn_layer)
 
 
-
-
 # ----------------------------------------------------------------------
 # Non-local CC
 
-import torch.autograd as autograd
-import torch.cuda.comm as comm
-import torch.nn.functional as F
-from torch.autograd.function import once_differentiable
-from torch.utils.cpp_extension import load
-import os, time
-import functools
-
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-_src_path = os.path.join(curr_dir, "src")
-_build_path = os.path.join(curr_dir, "build")
-os.makedirs(_build_path, exist_ok=True)
-rcca = load(name="rcca",
-            extra_cflags=["-O3"],
-            build_directory=_build_path,
-            verbose=True,
-            sources=[os.path.join(_src_path, f) for f in [
-                "lib_cffi.cpp", "ca.cu"
-            ]],
-            extra_cuda_cflags=["--expt-extended-lambda"])
-
-
-def _check_contiguous(*args):
-    if not all([mod is None or mod.is_contiguous() for mod in args]):
-        raise ValueError("Non-contiguous input")
-
-
-class CA_Weight(autograd.Function):
-    @staticmethod
-    def forward(ctx, t, f):
-        # Save context
-        n, c, d, h, w = t.size()
-        size = (n, h + w + d - 1, d, h, w)
-        weight = torch.zeros(size, dtype=t.dtype, layout=t.layout, device=t.device)
-
-        rcca.ca_forward_cuda(t, f, weight)
-
-        # Output
-        ctx.save_for_backward(t, f)
-
-        return weight
-
-    @staticmethod
-    @once_differentiable
-    def backward(ctx, dw):
-        t, f = ctx.saved_tensors
-
-        dt = torch.zeros_like(t)
-        df = torch.zeros_like(f)
-
-        rcca.ca_backward_cuda(dw.contiguous(), t, f, dt, df)
-
-        _check_contiguous(dt, df)
-
-        return dt, df
-
-
-class CA_Map(autograd.Function):
-    @staticmethod
-    def forward(ctx, weight, g):
-        # Save context
-        out = torch.zeros_like(g)
-        rcca.ca_map_forward_cuda(weight, g, out)
-
-        # Output
-        ctx.save_for_backward(weight, g)
-
-        return out
-
-    @staticmethod
-    @once_differentiable
-    def backward(ctx, dout):
-        weight, g = ctx.saved_tensors
-
-        dw = torch.zeros_like(weight)
-        dg = torch.zeros_like(g)
-
-        rcca.ca_map_backward_cuda(dout.contiguous(), weight, g, dw, dg)
-
-        _check_contiguous(dw, dg)
-
-        return dw, dg
-
-
-ca_weight = CA_Weight.apply
-ca_map = CA_Map.apply
-
-
-class CrissCrossAttention(nn.Module):
-    """ Criss-Cross Attention Module
-
-    ca = CrissCrossAttention(256).cuda()
-    x = torch.zeros(1, 8, 10, 10).cuda() + 1
-    y = torch.zeros(1, 8, 10, 10).cuda() + 2
-    z = torch.zeros(1, 64, 10, 10).cuda() + 3
-    out = ca(x, y, z)
-
-    """
-
-    def __init__(self, in_dim):
-        super(CrissCrossAttention, self).__init__()
-        self.chanel_in = in_dim
-
-        self.query_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
-        self.key_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
-        self.value_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-    def forward(self, x):
-        proj_query = self.query_conv(x)
-        proj_key = self.key_conv(x)
-        proj_value = self.value_conv(x)
-
-        energy = ca_weight(proj_query, proj_key)
-        attention = F.softmax(energy, 1)
-        out = ca_map(attention, proj_value)
-        out = self.gamma * out + x
-        return out
-
+# import torch.autograd as autograd
+# import torch.cuda.comm as comm
+# import torch.nn.functional as F
+# from torch.autograd.function import once_differentiable
+# from torch.utils.cpp_extension import load
+# import os, time
+# import functools
+#
+# curr_dir = os.path.dirname(os.path.abspath(__file__))
+# _src_path = os.path.join(curr_dir, "src")
+# _build_path = os.path.join(curr_dir, "build")
+# os.makedirs(_build_path, exist_ok=True)
+# rcca = load(name="rcca",
+#             extra_cflags=["-O3"],
+#             build_directory=_build_path,
+#             verbose=True,
+#             sources=[os.path.join(_src_path, f) for f in [
+#                 "lib_cffi.cpp", "ca.cu"
+#             ]],
+#             extra_cuda_cflags=["--expt-extended-lambda"])
+#
+#
+# def _check_contiguous(*args):
+#     if not all([mod is None or mod.is_contiguous() for mod in args]):
+#         raise ValueError("Non-contiguous input")
+#
+#
+# class CA_Weight(autograd.Function):
+#     @staticmethod
+#     def forward(ctx, t, f):
+#         # Save context
+#         n, c, d, h, w = t.size()
+#         size = (n, h + w + d - 1, d, h, w)
+#         weight = torch.zeros(size, dtype=t.dtype, layout=t.layout, device=t.device)
+#
+#         rcca.ca_forward_cuda(t, f, weight)
+#
+#         # Output
+#         ctx.save_for_backward(t, f)
+#
+#         return weight
+#
+#     @staticmethod
+#     @once_differentiable
+#     def backward(ctx, dw):
+#         t, f = ctx.saved_tensors
+#
+#         dt = torch.zeros_like(t)
+#         df = torch.zeros_like(f)
+#
+#         rcca.ca_backward_cuda(dw.contiguous(), t, f, dt, df)
+#
+#         _check_contiguous(dt, df)
+#
+#         return dt, df
+#
+#
+# class CA_Map(autograd.Function):
+#     @staticmethod
+#     def forward(ctx, weight, g):
+#         # Save context
+#         out = torch.zeros_like(g)
+#         rcca.ca_map_forward_cuda(weight, g, out)
+#
+#         # Output
+#         ctx.save_for_backward(weight, g)
+#
+#         return out
+#
+#     @staticmethod
+#     @once_differentiable
+#     def backward(ctx, dout):
+#         weight, g = ctx.saved_tensors
+#
+#         dw = torch.zeros_like(weight)
+#         dg = torch.zeros_like(g)
+#
+#         rcca.ca_map_backward_cuda(dout.contiguous(), weight, g, dw, dg)
+#
+#         _check_contiguous(dw, dg)
+#
+#         return dw, dg
+#
+#
+# ca_weight = CA_Weight.apply
+# ca_map = CA_Map.apply
+#
+#
+# class CrissCrossAttention(nn.Module):
+#     """ Criss-Cross Attention Module
+#     ca = CrissCrossAttention(256).cuda()
+#     x = torch.zeros(1, 8, 10, 10).cuda() + 1
+#     y = torch.zeros(1, 8, 10, 10).cuda() + 2
+#     z = torch.zeros(1, 64, 10, 10).cuda() + 3
+#     out = ca(x, y, z)
+#     """
+#
+#     def __init__(self, in_dim):
+#         super(CrissCrossAttention, self).__init__()
+#         self.chanel_in = in_dim
+#
+#         self.query_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
+#         self.key_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim // 2, kernel_size=1)
+#         self.value_conv = nn.Conv3d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+#         self.gamma = nn.Parameter(torch.zeros(1))
+#
+#     def forward(self, x):
+#         proj_query = self.query_conv(x)
+#         proj_key = self.key_conv(x)
+#         proj_value = self.value_conv(x)
+#
+#         energy = ca_weight(proj_query, proj_key)
+#         attention = F.softmax(energy, 1)
+#         out = ca_map(attention, proj_value)
+#         out = self.gamma * out + x
+#         return out
