406b3fbd6d5b85e03a5a7d518fd37f5db13c9c1f
anonymous <641475073@qq.com>
2021-09-29 18:29:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PATH=/opt/conda/bin:$PATH &&\
-cd /braindat/lab/limx/MitoEM2021/CODE/Author/residual_unet/28_36_48_64_80_non_local/connectomics/model/block &&\
-python setup.py install &&\
-cd /braindat/lab/limx/MitoEM2021/CODE/HUMAN/rsunet_retrain &&\
-python scripts/main.py --config-file configs/MitoEM/MitoEM-R-BC.yaml --checkpoint /braindat/lab/limx/MitoEM2021/CODE/HUMAN/rsunet_retrain/outputs/dataset_output/checkpoint_162500.pth.tar
+
+cd xxx &&\ # please change it
+python scripts/main.py --config-file configs/MitoEM/MitoEM-R-BC.yaml 
